7063bfaabba82bb57df81b04292e9b9701090318
Ritish <ritishs@perion.com>
2025-08-22 13:17:04 +0530

    chore: remove Vercel serverless wrappers under server/api/* (clean non-Vercel deployment)

--- server/api/[...path].js
+++
@@ -1,8 +0,0 @@
-// Vercel serverless function that forwards all requests to our Express app
-const app = require('../index')
-
-module.exports = (req, res) => {
-  // Vercel provides req.url including the path under /api, our Express app is mounted at root
-  return app(req, res)
-}
-
--- server/api/auth/request-otp.js
+++
@@ -1,5 +0,0 @@
-// Explicit route wrapper for Vercel so OPTIONS/POST hit Express
-const app = require('../../index')
-
-module.exports = (req, res) => app(req, res)
-

e913bd7d1d3c61b2bfc19267d3be2698a53311fd
Ritish <ritishs@perion.com>
2025-08-23 23:53:53 +0530

    Revert "vercel: add explicit api/auth/request-otp function mapping to Express to satisfy preflight+POST"
    This reverts commit 4f1b7377effc665d85d862074786d231e3b3841c.

--- server/api/auth/request-otp.js
+++
@@ -1,5 +0,0 @@
-// Explicit function for /api/auth/request-otp to ensure Vercel routes preflights and POST here
-const serverless = require('serverless-http')
-const app = require('../../index.js')
-module.exports = serverless(app)
-

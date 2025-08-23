093dc11c31d1e053265b5dc6e578050c5beb7104
Ritish <ritishs@perion.com>
2025-08-23 23:53:53 +0530

    Revert "vercel error fixed"
    This reverts commit 8b2592a1d10abebfeab3041897fd9361b6cfe0f7.

--- server/api/[...all].js
+++
@@ -1,6 +0,0 @@
-// Vercel catch-all API route to serve the Express app for all /api/* paths
-// This ensures routes like /api/health, /api/orders, etc., are handled by Express
-
-const app = require('../index.js')
-module.exports = app
-

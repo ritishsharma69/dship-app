2d77fd8174e87e2353bb2e306ce1eb797c053dd2
Ritish <ritishs@perion.com>
2025-09-13 08:48:00 +0530

    fix(admin-returns): remove unused default React import to satisfy noUnusedLocals

--- my-app/src/pages/AdminReturnsPage.tsx
+++ my-app/src/pages/AdminReturnsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { apiGetJson, apiPostJson } from '../lib/api'
 import { useRouter } from '../lib/router'
 
